30e6c4418491c8be6689018b86b15a4e266b0aa8
James Allardice <james@allardice.me>
2017-08-15 20:18:55 +0100

    Attempt to get stored credentials from the browser
    If the browser supports the Credential Management API we now ask for
    stored credentials and attempt to log the user in automatically.

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -11,7 +11,8 @@ if (loginForm) {
   }
 
   function login(form) {
-    const formData = new FormData(form);
+    const isFormData = form instanceof FormData;
+    const formData = isFormData ? form : new FormData(form);
 
     // Make a POST request to the login endpoint. We are expecting JSON in
     // response rather than the HTML we would expect if the form was submitted
@@ -33,7 +34,11 @@ if (loginForm) {
       // suggest that it remembers the credentials used to successfully sign in
       // at this point.
       if (res.success) {
-        if (navigator.credentials) {
+        // If the login function has been invoked with a FormData instance
+        // rather than a form node we know that we're logging in with a
+        // credential handed to us by the browser. We don't need to attempt to
+        // save it since the browser already has it stored.
+        if (navigator.credentials && !isFormData) {
           // Create a PasswordCredential instance from the form and ask the
           // browser to prompt the user to save it.
           const credential = new PasswordCredential(form);
@@ -60,4 +65,26 @@ if (loginForm) {
     // Make a request to the login backend and handle the response.
     login(loginForm);
   });
+
+  // If the browser offers support for the Credential Management API we can ask
+  // it for a password credential stored against our origin.
+  if (navigator.credentials) {
+    navigator.credentials.get({
+      password: true,
+    })
+    .then((credential) => {
+      // If the browser has a password credential to offer we can build up a
+      // FormData instance and make a login request with it. If no credential
+      // was available we do nothing. The user will be able to submit the login
+      // form like usual.
+      if (credential) {
+        const formData = new FormData();
+
+        formData.append(credential.idName, credential.id);
+        formData.append(credential.passwordName, credential.password);
+
+        login(formData);
+      }
+    });
+  }
 }
